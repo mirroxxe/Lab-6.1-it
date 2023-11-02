a4d7c7abc2bccd7048cb98cec47c171f57fa80a3
Volodya <chuchman002@gmail.com>
2023-11-02 12:34:53 +0200

    Add project files.

---
+++ Lab 6.1 it/Lab 6.1 it.cpp
@@ -0,0 +1,69 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+const int arraySize = 24;
+const int lowerLimit = -50;
+const int upperLimit = 15;
+
+void generateRandom(int arr[], int size, int lower, int upper) {
+    for (int i = 0; i < size; i++) {
+        arr[i] = rand() % (upper - lower + 1) + lower;
+    }
+}
+
+void print(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        std::cout << arr[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+int countNegative(int arr[], int size) {
+    int count = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0 || arr[i] % 2 != 0) {
+            count++;
+        }
+    }
+    return count;
+}
+
+int sumNegative(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0 || arr[i] % 2 != 0) {
+            sum += arr[i];
+        }
+    }
+    return sum;
+}
+
+void replaceNegative(int arr[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (arr[i] < 0 || arr[i] % 2 != 0) {
+            arr[i] = 0;
+        }
+    }
+}
+
+int main() {
+    int arr[arraySize];
+    srand(static_cast<unsigned>(time(0)));
+
+    generateRandom(arr, arraySize, lowerLimit, upperLimit);
+    std::cout << "Generated array: ";
+    print(arr, arraySize);
+
+    int count = countNegative(arr, arraySize);
+    int sum = sumNegative(arr, arraySize);
+
+    std::cout << "Count of negative: " << count << std::endl;
+    std::cout << "Sum of negative: " << sum << std::endl;
+
+    replaceNegative(arr, arraySize);
+    std::cout << "Modified array: ";
+    print(arr, arraySize);
+
+    return 0;
+}
---
+++ UnitTest1/UnitTest1.cpp
@@ -0,0 +1,35 @@
+﻿#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Lab 6.1 it/Lab 6.1 it.cpp" 
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace UnitTest
+{
+	TEST_CLASS(UnitTest)
+	{
+	public:
+
+		TEST_METHOD(TestGenerateRandom)
+		{
+			int arr[arraySize];
+			generateRandom(arr, arraySize, lowerLimit, upperLimit);
+
+			for (int i = 0; i < arraySize; i++) {
+				Assert::IsTrue(arr[i] >= lowerLimit && arr[i] <= upperLimit);
+			}
+		}
+		
+
+		TEST_METHOD(TestReplaceNegative)
+		{
+			int arr[] = { -3, 4, -5, 7, -2, 10 };
+			int size = 6;
+
+			replaceNegative(arr, size);
+
+			for (int i = 0; i < size; i++) {
+				Assert::IsTrue(arr[i] >= 0);
+			}
+		}
+	};
+}
